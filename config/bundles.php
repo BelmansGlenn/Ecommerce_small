42205125c4b859b1d3222009e31142b04f8d7716
BelmansGlenn <belmansglenn@gmail.com>
2021-10-19 13:57:30 +0200

    EasyAdmin USer/Category/Products

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
