4ba62cea51814623fdd650041eb5eff4ca3d40b7
BelmansGlenn <belmansglenn@gmail.com>
2021-10-23 12:07:07 +0200

    Filter category on the homepage, and display different footer on different page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,39 @@
 
 namespace App\Controller;
 
+use App\Classes\Search;
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Form\SearchType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+
+    private EntityManagerInterface $entityManager;
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
     /**
      * @Route("/", name="home")
      */
     public function index(): Response
     {
+        $filterCategory = $this->entityManager->getRepository(Category::class)->findAll();
+
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'categories' => $filterCategory
         ]);
     }
 }
