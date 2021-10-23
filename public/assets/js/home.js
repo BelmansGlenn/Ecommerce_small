4057b6c802d64141fc06b0f46fb7fce43649cb7c
AyraStelmaszewski <ayrastelmaszewski@gmail.com>
2021-10-23 16:09:10 +0200

    home fix

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -27,9 +27,46 @@ var promoAnim = {
   const clcBtn = document.querySelector(".clcBtn")
   const ecgBtn = document.querySelector(".ecgBtn")
 
-  function filterShop() {
-    clcBtn.addEventListener('click', (el) =>{ 
-      alert("gg")
+function clcCaroussel() {
+    let clcImg = ["home.jpg", "room.jpg", "decoration.jpg", "exterieur.jpg"]
+    let clcTxt = ["Salon", "Chambre", "Décoration", "Extérieur"]
+    let clcBox = document.querySelector(".jsClc")
+    let i = 1
+    clcBtn.addEventListener('click', (el) => { 
+      clcBox.innerHTML = `
+        <figure><img src="/assets/img/${clcImg[i]}"></figure>
+        <section>
+            <h2>${clcTxt[i]}</h2>
+            <a href="{{ path('products') }}">shop now</a>
+        </section>
+      `
+      i++;
+      if(i === 4) {
+        i = 0
+      }
+      console.log(clcImg[1]);
+      console.log(clcBox);
     })
 }
-filterShop()
+clcCaroussel()
+
+function ecgCaroussel() {
+  let ecgImg = ["lightSalon.jpg", "roomecg.jpg", "deskecg.jpg", "salonecg.jpg"]
+  let ecgTxt = ["Intérieur", "Chambre", "Bureaux", "Salon"]
+  let ecgBox = document.querySelector(".jsEcg")
+  let i = 1
+  ecgBtn.addEventListener('click', (el) => { 
+    ecgBox.innerHTML = `
+      <figure><img src="/assets/img/${ecgImg[i]}"></figure>
+      <section>
+          <h2>${ecgTxt[i]}</h2>
+          <a href="{{ path('products') }}">shop now</a>
+      </section>
+    `
+    i++;
+    if(i === 4) {
+      i = 0
+    }
+  })
+}
+ecgCaroussel()
