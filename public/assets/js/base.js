8186bc85c605ba4bbd7b15f18460c338ae05b46d
AyraStelmaszewski <ayrastelmaszewski@gmail.com>
2021-10-28 21:13:54 +0200

    cursor

--- public/assets/js/base.js
+++ public/assets/js/base.js
@@ -35,3 +35,38 @@ function menuClick() {
 }
 menuClick()
 
+
+// CURSOR 
+const point = document.querySelector('.cursor--point');
+document.addEventListener("mousemove", cursorPos, false);
+function cursorPos(event) {
+  pageX = event.pageX;
+  pageY= event.pageY;
+  point.style.transform = 'translate3d(' + (pageX - 1) + 'px ,' + (pageY -1) + 'px, 0)';;
+}
+
+
+// Take activies input end
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+// CURSOR 
+const cursor = document.querySelector('#cursor');
+// const point = document.querySelector('.cursor--point');
+let mouse = { x: 300, y: 300 };
+let pos = { x: 0, y: 0 };
+const speed = 0.1; // between 0 and 1
+const updatePosition = () => {
+  pos.x += (mouse.x - pos.x) * speed;
+  pos.y += (mouse.y - pos.y) * speed;
+  cursor.style.transform = 'translate3d(' + pos.x + 'px ,' + pos.y + 'px, 0)';
+}
+const updateCoordinates = e => {
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+}
+window.addEventListener('mousemove', updateCoordinates);
+function loop() {
+  updatePosition();
+  requestAnimationFrame(loop);
+}
+requestAnimationFrame(loop);
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
