6404a60a823e4f3a966c5265dd5d4592383068af
AyraStelmaszewski <ayrastelmaszewski@gmail.com>
2021-11-01 15:09:34 +0100

    fix gotop routes .ico

--- public/assets/js/base.js
+++ public/assets/js/base.js
@@ -1,24 +1,38 @@
+// Return top 
+
 const goBackBtn = document.querySelector("#goTop")
 goBackBtn.addEventListener('click', (e) => {
-    gaoTop()
-})
-
-    
-function gaoTop() {
+    goTop()
+}) 
+function goTop() {
     window.scrollTo({top: 0, behavior: 'smooth'});
     goBackBtn.style.animation = "popOff 1s ease-in forwards"
 }
-
-// Scroll detector 
 window.onscroll = function() {
     checkScroll()
 };
-
 function checkScroll() {
   if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
     goBackBtn.style.display = "inline-grid"
+    goBackBtn.style.animation = "popOn 1s ease-in forwards"
+  } else if (document.body.scrollTop < 350 || document.documentElement.scrollTop < 350) {
+    goBackBtn.style.animation = "popOff 1s ease-in forwards"
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // MENU 
 menuBtn = document.querySelector(".menuOpen")
